9e91559785813673a36d5b191b6fffc2f2cc11cc
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-08-16 11:24:54 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'SignUp/loginpage.dart';
 import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
